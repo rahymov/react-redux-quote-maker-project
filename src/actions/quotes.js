0f7a624e4dd2c3f567b1d51993f7edc4864b5444
Yazgylych Rahymov <y.rahymov@gmail.com>
2020-05-21 16:57:38 -0400

    react redux quote maker app

---
+++ src/actions/quotes.js
@@ -0,0 +1,27 @@
+export const addQuote = (quote) => {
+  return{
+    type: 'ADD_QUOTE',
+    quote: Object.assign({}, quote, {votes: 0})
+  }
+}
+
+export const removeQuote = (quoteId) => {
+  return {
+    type: 'REMOVE_QUOTE',
+    quoteId
+  }
+}
+
+export const upvoteQuote = quoteId => {
+  return {
+    type: 'UPVOTE_QUOTE',
+    quoteId
+  }
+}
+
+export const downvoteQuote = quoteId => {
+  return{
+    type: 'DOWNVOTE_QUOTE',
+    quoteId
+  }
+}
